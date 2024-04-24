d6c8afe622d840e714c4fd74083efcdb3ec96a36
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2024-04-24 21:45:21 +0200

    Note colouring!

--- src/notation/internal/notationmidiinput.cpp
+++ src/notation/internal/notationmidiinput.cpp
@@ -105,6 +105,7 @@ void NotationMidiInput::rewind() {
   m_currentMeasure = nullptr;
   m_currentChordRestSegment = nullptr;
   playbackController()->seek(int64_t{0});
+  score()->deselectAll();
 }
 
 mu::engraving::Score* NotationMidiInput::score() const
@@ -147,7 +148,7 @@ void NotationMidiInput::doProcessEvents()
         return;
     }
 
-    std::vector<const Note*> notes;
+    std::vector<Note*> notes;
 
     auto gain = 0.0;
     for (size_t i = 0; i < m_eventsQueue.size(); ++i) {
@@ -212,13 +213,18 @@ void NotationMidiInput::doProcessEvents()
     }
 
     if (!notes.empty()) {
-        std::vector<const EngravingItem*> notesItems;
-        for (const Note* note : notes) {
+        std::vector<EngravingItem*> notesItems;
+        for (Note* note : notes) {
             notesItems.push_back(note);
         }
 
-        playbackController()->playElements(notesItems, gain);
-        m_notesReceivedChannel.send(notes);
+        playbackController()->playElements(
+            {notesItems.begin(), notesItems.end()}, gain);
+        m_notesReceivedChannel.send({notes.begin(), notes.end()});
+
+        auto pScore = score();
+        pScore->deselectAll();
+        pScore->select(notesItems, engraving::SelectType::ADD);
     }
 
     m_eventsQueue.clear();
