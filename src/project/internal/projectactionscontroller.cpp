6c713e067e784015ceea7d3d958166920ddfadd4
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-11-06 09:35:26 +0100

    Modify warning when opening too recent version MSS file

--- src/project/internal/projectactionscontroller.cpp
+++ src/project/internal/projectactionscontroller.cpp
@@ -1785,9 +1785,8 @@ bool ProjectActionsController::askIfUserAgreesToOpenProjectWithIncompatibleVersi
 void ProjectActionsController::warnFileTooNew(const muse::io::path_t& filepath)
 {
     interactive()->error(muse::qtrc("project", "Cannot read file %1").arg(io::toNativeSeparators(filepath).toQString()).toStdString(),
-                         muse::mtrc("project", "This file was saved using a newer version of MuseScore Studio. "
-                                               "Please visit <a href=\"%1\">MuseScore.org</a> to obtain the latest version.")
-                         .arg(u"https://musescore.org").toStdString());
+                         muse::trc("project", "This file MuseScore Studio file is of a recent, yet unsupported version. "
+                                              "Open it first in MuseScore Studio, export it in MusicXML format and open it in Orchestrion."));
 }
 
 bool ProjectActionsController::askIfUserAgreesToOpenCorruptedProject(const String& projectName, const std::string& errorText)
